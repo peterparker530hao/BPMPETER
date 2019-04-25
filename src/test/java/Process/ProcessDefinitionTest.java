4114b5d9cca79a70df90f222cf8cb9f0b6396603
peterparker530hao <tangjia_email@163.com>
2019-04-25 10:48:39 +0800

    更新测试

--- src/test/java/Process/ProcessDefinitionTest.java
+++ src/test/java/Process/ProcessDefinitionTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.InputStream;
 import java.util.zip.ZipInputStream;
-
+//更新测试
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:spring-mvc.xml",
         "classpath*:mybatis-config.xml","classpath*:applicationContext.xml","classpath*:activiti-context.xml"})
